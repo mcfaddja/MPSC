b2e6ba3297ef835bd43b4254ca229ab3a3b49d08
mcfaddja <mcfaddja@uw.edu>
2017-05-21 11:23:42 -0700

    GeneralFileReader variables and constructor
    Finished coding the class level variables, as well as the constructor,
    for the GeneralFileReader class.
    
    Started on the the public methods with utility methods for getting and
    setting the name of the file to be read; however, as allowing the
    filename to be changed after instantiation of the class as a whole
    would prevent the variable holding the filename from being declared
    “final”.  Therefore, I will soon be removing the setter method.

--- NetBeans/MPSC/src/files/GeneralFileReader.java
+++ NetBeans/MPSC/src/files/GeneralFileReader.java
@@ -5,10 +5,59 @@
  */
 package files;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author jamster
+ * Provides a file reading utility class to read from an input file, specified 
+ *  by its filename, and store the resulting data in a String array.  
+ * 
+ * @author Jonathan McFadden (mcfaddja@uw.edu)
+ * @version 0.1
  */
 public class GeneralFileReader {
+    
+    /** String to hold the name of the file to be read. */
+    private final String myFileName;
+    
+    /** An ArrayList of String to store the data as it is read from the file. */
+    private final List<String> myInput;
+    
+    
+    
+    /**
+     * Creates a reader with methods to read the file specified by the passed
+     * filename as well as to return the data read from the file in the form of
+     * a String array.
+     * 
+     * @param theFileName The filename of the file to be read.
+     */
+    public GeneralFileReader(final String theFileName) {
+        myFileName = theFileName;
+        
+        myInput = new ArrayList<String>();
+    } // END constructor
+    
+    
+    
+    /**
+     * Utility method to return the filename currently specified for use by 
+     *  the reader.
+     * 
+     * @return myFileName
+     */
+    public String getFileName() {
+        return myFileName;
+    }
+    
+    
+    /**
+     * Utility method to change the filename currently specified for use by 
+     *  the reader.
+     */
+    public void setFileName(final String theFileName) {
+        myFileName = theFileName;
+    }
+    
     
 }
